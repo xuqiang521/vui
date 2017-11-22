d973cd32e2c27b9da80226b79c32f68e55da749a
xuqiang521 <1257875587@qq.com>
2017-11-22 10:05:46 +0800

    🚀  improved route loading(lazy loading routes)

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -1,34 +1,54 @@
+import App from '../App'
+
+const home     = r => require.ensure([], () => r(require('pages/index')), 'home')
+const dropdown = r => require.ensure([], () => r(require('pages/dropdown')), 'dropdown')
+const scroller = r => require.ensure([], () => r(require('pages/scroller')), 'scroller')
+const search   = r => require.ensure([], () => r(require('pages/search')), 'search')
+const swiper   = r => require.ensure([], () => r(require('pages/swiper')), 'swiper')
+const table    = r => require.ensure([], () => r(require('pages/table')), 'table')
+
 let routers = [];
 routers = [
   {
-    path: '/home',
-    name: 'index',
-    component: require('pages/index')
-  },
-  {
-    path: '/dropdown',
-    name: 'dropdown',
-    component: require('pages/dropdown'),
-  },
-  {
-    path: '/scroller',
-    name: 'scroller',
-    component: require('pages/scroller'),
-  },
-  {
-    path: '/search',
-    name: 'search',
-    component: require('pages/search'),
-  },
-  {
-    path: '/swiper',
-    name: 'swiper',
-    component: require('pages/swiper'),
-  },
-  {
-    path: '/table',
-    name: 'table',
-    component: require('pages/table'),
+    path: '/',
+    component: App,
+    children: [
+      //地址为空时跳转home页面
+      {
+        path: '',
+        redirect: '/home'
+      },
+      {
+        path: 'home',
+        name: 'home',
+        component: home
+      },
+      {
+        path: 'dropdown',
+        name: 'dropdown',
+        component: dropdown,
+      },
+      {
+        path: 'scroller',
+        name: 'scroller',
+        component: scroller,
+      },
+      {
+        path: 'search',
+        name: 'search',
+        component: search,
+      },
+      {
+        path: 'swiper',
+        name: 'swiper',
+        component: swiper,
+      },
+      {
+        path: 'table',
+        name: 'table',
+        component: table,
+      }
+    ]
   }
 ]
 
